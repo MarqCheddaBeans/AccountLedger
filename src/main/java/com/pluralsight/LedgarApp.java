632fae0c846925a6b6d4ed7c1cf21e241e7a0a7e
IsaiahMV27 <mvillanueva@my.yearupunited.org>
2025-10-10 12:36:30 -0400

    First commit. Created a log in system.

---
+++ src/main/java/com/pluralsight/LedgarApp.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class LedgarApp {
+
+    //Create scanner that is class wide
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String validUser = "Marques123";
+        String validPass = "notPassword";
+
+        System.out.println("Please log in to your account.");
+        System.out.print("Username: ");
+        String userName = scan.nextLine();
+
+        while(!userName.equalsIgnoreCase(validUser) ){
+            System.out.println("Invalid username. Try again!");
+            System.out.print("Username: ");
+            userName = scan.nextLine();
+        }
+        System.out.print("\nPassword: ");
+        String password = scan.nextLine();
+
+        while(!password.equals(validPass)){
+            System.out.println("Invalid password. Try again!");
+            System.out.print("Password: ");
+            password = scan.nextLine();
+        }
+
+
+    }
+}
