5130a62494fcdc6d878c564c406b0d9f0123a819
IsaiahMV27 <mvillanueva@my.yearupunited.org>
2025-10-11 22:58:44 -0400

    Added option to sort transactions by previous year

--- src/main/java/com/pluralsight/LedgarApp.java
+++ src/main/java/com/pluralsight/LedgarApp.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -71,6 +72,12 @@ public class LedgarApp {
                         //sort previous month
                         sortPreviousMonth();
                     }
+                    else if(reportInput == 3){
+                        //sort year to date
+                    }
+                    else if(reportInput == 4){
+                        sortPreviousYear();
+                    }
 
                     }
             //User chose to return to main menu/ homepage
@@ -84,6 +91,58 @@ public class LedgarApp {
                 System.exit(0);
             }
     }
+
+    private static void sortPreviousYear(){
+
+        ArrayList<Transaction> prevYearList = new ArrayList<>();
+
+        LocalDate today = LocalDate.now();
+        LocalDate prevYearDate = today.minusYears(1);
+        int prevYear = prevYearDate.getYear();
+
+        try(BufferedReader buffWrite = new BufferedReader(new FileReader("transactions.csv"))){
+
+            String line;
+            while((line = buffWrite.readLine()) != null) {
+                String[] transInfo = line.split("\\|");
+
+                if (transInfo[0].trim().equalsIgnoreCase("Date")) {
+                    continue;
+                }
+
+                try {
+
+                    LocalDate date = LocalDate.parse(transInfo[0].trim());
+                    LocalTime time = LocalTime.parse(transInfo[1].trim());
+                    String description = transInfo[2].trim();
+                    String vendor = transInfo[3].trim();
+                    double amount = Double.parseDouble(transInfo[4].trim());
+
+                    if (date.getYear() == prevYear) {
+                        prevYearList.add(new Transaction(date, time, description, vendor, amount));
+                    }
+
+                } catch (NumberFormatException e) {
+                    System.out.println("You done did it");
+                }
+            }
+                prevYearList.sort(Comparator.comparing(Transaction::getDate).thenComparing(Transaction::getTime).reversed());
+
+                //Create String named header to add header to be displayed to user
+                String header = ( "Date\t\t| Time\t\t | Description\t\t\t\t\t\t\t\t\t\t | Vendor\t\t\t\t |   Amount");
+                System.out.println(header);
+
+                //Loop through all Transaction objects in the payment ArrayList
+                for(Transaction t: prevYearList){
+                    //Print out formatted version of information contained in Transaction defined as t, (%-12s = Left align String with 12 character space, %10.2f = Right aligned float with 10 character space and shows 2 decimal points)
+                    System.out.printf("%-12s| %-11s| %-50s| %-22s|%10.2f\n", t.getDate(), t.getTime(), t.getDescription(), t.getVendor(), t.getAmount());
+                }
+        }
+            catch(IOException e){
+                System.out.println("Oops");
+        }
+    }
+
 
     private static void sortPreviousMonth(){
 
@@ -505,7 +564,7 @@ public class LedgarApp {
         System.out.println("Please log in to your account.");
 
         for(int attempt = 3; attempt > 0; attempt--){
-            System.out.print("Username: ");
+            System.out.print("Enter Username: ");
             String userName = scan.nextLine();
 
             if(userName.equalsIgnoreCase(validUser)){
@@ -522,7 +581,7 @@ public class LedgarApp {
 
         for(int attempt = 3; attempt > 0; attempt--){
 
-            System.out.print("Please enter password: ");
+            System.out.print("Enter password: ");
             String password = scan.nextLine();
 
             if(password.equals(validPass)){
